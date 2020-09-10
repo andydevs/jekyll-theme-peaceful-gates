26f20d6511569ed91db879eacb03796636653673
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-10 02:21:04 -0400

    Auto toggle darkmode, maybe?

--- assets/js/darkmode-toggle.js
+++ assets/js/darkmode-toggle.js
@@ -1,40 +1,60 @@
 $(function() {
     // Get body jQuery
     let $body = $('body')
+    let $auto = $('.show-on-auto')
 
+    /**
+     * Enable darkmode
+     */
     function darkmodeEnable() {
+        $auto.hide()
         $body.addClass('darkmode')
         console.log('Darkmode enabled!')
     }
 
+    /**
+     * Disable darkmode
+     */
     function darkmodeDisable() {
+        $auto.hide()
         $body.removeClass('darkmode')
         console.log('Darkmode disabled!')
     }
 
-    // Darkmode false/true
-    let darkmode = localStorage.getItem('darkmode') || 'off'
+    /**
+     * Darkmode on auto
+     */
+    function darkmodeAuto() {
+        $auto.show()
+        let isdarkmode = window.matchMedia('prefers-color-scheme: dark').matches
+        $body.toggleClass('darkmode', isdarkmode)
+        console.log('Darkmode on auto!')
+    }
+
+    // Get darkmode from localstorage
+    let darkmode = localStorage.getItem('darkmode') || 'auto'
     console.log('Darkmode:', darkmode)
 
-    // Toggle initial darkmode
-    if (darkmode === 'on') {
-        darkmodeEnable();
-    }
-    else {
-        darkmodeDisable();
-    }
+    // Set initial darkmode
+    if (darkmode === 'auto') darkmodeAuto();
+    else if (darkmode === 'on') darkmodeEnable();
+    else darkmodeDisable();
 
     // Darkmode toggle
     $('#toggle-darkmode').click(function() {
         console.group('Darkmode Toggle')
         console.log('Current darkmode:', darkmode)
-        if (darkmode == 'on') {
+        if (darkmode === 'auto') {
+            darkmodeEnable();
+            darkmode = 'on';
+        }
+        else if (darkmode === 'on') {
             darkmodeDisable();
             darkmode = 'off';
         }
         else {
-            darkmodeEnable();
-            darkmode = 'on';
+            darkmodeAuto()
+            darkmode = 'auto'
         }
         console.log('Next darkmode:', darkmode)
         localStorage.setItem('darkmode', darkmode)
