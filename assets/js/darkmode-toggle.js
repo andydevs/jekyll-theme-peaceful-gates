74cc455caa04eb6c32d7eb277aeb1afb4974bbbc
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-09 22:36:57 -0400

    Darkmode persists across sessions!

--- assets/js/darkmode-toggle.js
+++ assets/js/darkmode-toggle.js
@@ -1,5 +1,43 @@
 $(function() {
+    // Get body jQuery
+    let $body = $('body')
+
+    function darkmodeEnable() {
+        $body.addClass('darkmode')
+        console.log('Darkmode enabled!')
+    }
+
+    function darkmodeDisable() {
+        $body.removeClass('darkmode')
+        console.log('Darkmode disabled!')
+    }
+
+    // Darkmode false/true
+    let darkmode = localStorage.getItem('darkmode') || 'off'
+    console.log('Darkmode:', darkmode)
+
+    // Toggle initial darkmode
+    if (darkmode === 'on') {
+        darkmodeEnable();
+    }
+    else {
+        darkmodeDisable();
+    }
+
+    // Darkmode toggle
     $('#toggle-darkmode').click(function() {
-        $('body').toggleClass('darkmode')
+        console.group('Darkmode Toggle')
+        console.log('Current darkmode:', darkmode)
+        if (darkmode == 'on') {
+            darkmodeDisable();
+            darkmode = 'off';
+        }
+        else {
+            darkmodeEnable();
+            darkmode = 'on';
+        }
+        console.log('Next darkmode:', darkmode)
+        localStorage.setItem('darkmode', darkmode)
+        console.groupEnd()
     })
 })
