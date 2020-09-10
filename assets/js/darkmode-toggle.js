378cf75e99d28b2ceec8cc2051f8cc1bdd32a4c9
Anshul Kharbanda <akanshul97@gmail.com>
2020-09-10 02:38:21 -0400

    Never mind

--- assets/js/darkmode-toggle.js
+++ assets/js/darkmode-toggle.js
@@ -1,13 +1,11 @@
 $(function() {
     // Get body jQuery
     let $body = $('body')
-    let $auto = $('.show-on-auto')
 
     /**
      * Enable darkmode
      */
     function darkmodeEnable() {
-        $auto.hide()
         $body.addClass('darkmode')
         console.log('Darkmode enabled!')
     }
@@ -16,45 +14,29 @@ $(function() {
      * Disable darkmode
      */
     function darkmodeDisable() {
-        $auto.hide()
         $body.removeClass('darkmode')
         console.log('Darkmode disabled!')
     }
 
-    /**
-     * Darkmode on auto
-     */
-    function darkmodeAuto() {
-        $auto.show()
-        let isdarkmode = window.matchMedia('prefers-color-scheme: dark').matches
-        $body.toggleClass('darkmode', isdarkmode)
-        console.log('Darkmode on auto!')
-    }
-
     // Get darkmode from localstorage
-    let darkmode = localStorage.getItem('darkmode') || 'auto'
+    let darkmode = localStorage.getItem('darkmode') || 'off'
     console.log('Darkmode:', darkmode)
 
     // Set initial darkmode
-    if (darkmode === 'auto') darkmodeAuto();
-    else if (darkmode === 'on') darkmodeEnable();
+    if (darkmode === 'on') darkmodeEnable();
     else darkmodeDisable();
 
     // Darkmode toggle
     $('#toggle-darkmode').click(function() {
         console.group('Darkmode Toggle')
         console.log('Current darkmode:', darkmode)
-        if (darkmode === 'auto') {
-            darkmodeEnable();
-            darkmode = 'on';
-        }
-        else if (darkmode === 'on') {
+        if (darkmode === 'on') {
             darkmodeDisable();
             darkmode = 'off';
         }
         else {
-            darkmodeAuto()
-            darkmode = 'auto'
+            darkmodeEnable()
+            darkmode = 'on'
         }
         console.log('Next darkmode:', darkmode)
         localStorage.setItem('darkmode', darkmode)
